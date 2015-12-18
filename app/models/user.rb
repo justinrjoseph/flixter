c566aa778b8927e89e297e9686334fb6342e6ca1
Justin Joseph <justin.joseph0@yahoo.com>
2015-12-18 03:00:36 +0000

    Add student enrollment to courses

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
+  
+  def enrolled_in?(course)
+    enrolled_courses.include? course
+  end
 end
